c00d1078e7759865418205eeef78dd2fbf9b4323
rabdiego <diegorabelodesa@gmail.com>
2023-06-17 15:14:16 -0300

    comecamos o trabalho

---
+++ ObservableSimulator.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public interface ObservableSimulator {
+    public void simulate(int starting_dimension, int ending_dimension, int step, int number_of_points, char distribution);
+    public ArrayList <Double> getValue();
+    public void addObserver(SimulatorObserver observer);
+    public void removeObserver(SimulatorObserver observer);
+}
