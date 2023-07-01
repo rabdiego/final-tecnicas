c922c34b07c6363f88efba221a4fa612cfebbbe0
ArtroxGabriel <artroxgabriel@gmail.com>
2023-07-01 17:11:57 -0300

    mudancas do Ramon

--- App.java
+++ App.java
@@ -11,10 +11,11 @@ public class App {
     private static XYChart chart;
 
     public static void main(String[] args){
-
+        
+        String[] values = new String[5];
         //Container Frame
-        f = new Frame("Titulo");
-        f.setSize(800,600);
+        Frame f = new Frame("Doidera");
+        f.setSize(1000,600);
         f.setBackground(null);
         //f.setForeground(Color.red);
         f.setResizable(true);
@@ -29,8 +30,18 @@ public class App {
         //Container Painel
         Panel pSouth = new Panel(); //layout manager dele é o flow layout
         pSouth.setBackground(Color.LIGHT_GRAY);
-        pSouth.setLayout(new FlowLayout(0,4,5));
-
+        pSouth.setLayout(new BorderLayout());
+        
+        //Panel que contem os inputs
+        Panel pSouthW = new Panel();
+        pSouthW.setLayout(new FlowLayout(0, 0, 10));
+        pSouthW.setBackground(Color.LIGHT_GRAY);
+        
+        //Panel que contem distribuição e botão 'ok'
+        Panel pSouthE = new Panel();
+        pSouthE.setLayout(new FlowLayout(0, 5, 10));
+        pSouthE.setBackground(Color.LIGHT_GRAY);
+    
         Panel pNorth = new Panel();
         pNorth.setBackground(Color.LIGHT_GRAY);
         pNorth.setLayout(new FlowLayout(0,2,10));
@@ -42,13 +53,12 @@ public class App {
         Panel pEast = new Panel();
         pEast.setBackground(Color.LIGHT_GRAY);
 
-
+        
         //Labels>
-        Label none0 = new Label("                   ");
-        Label none01 = new Label("      Distribuição:");
-        none01.setFont(new Font("MonoSpaced", Font.PLAIN, 14));
+        Label none01 = new Label("Distribuição:");
+        //none01.setFont(new Font("MonoSpaced", Font.PLAIN, 14));
 
-        Label algo = new Label("Algo:");
+        Label algo = new Label("Simulador Bolado:");
         algo.setFont(new Font("MonoSpaced", Font.BOLD, 16));
 
         Label input01Label = new Label("Inicio:", Label.CENTER);
@@ -60,8 +70,11 @@ public class App {
         Label input03Label = new Label("Passo:", Label.CENTER);
         input03Label.setBounds(100,100,160,20);
 
-        Label input04Label = new Label("Numero de Pontos:", Label.CENTER);
-        input04Label.setBounds(100,100,160,20);
+        Label input04Label = new Label("Pontos:", Label.CENTER);
+        input03Label.setBounds(100,100,160,20);
+
+        //Label input04Label = new Label("Input 04:", Label.CENTER);
+        //input02Label.setBounds(100,100,160,20);
 
         //Text Field Input
         TextField input01 = new TextField(null, 3);
@@ -80,65 +93,96 @@ public class App {
         b2.setCursor(new Cursor(Cursor.HAND_CURSOR));
         b2.setFont(new Font("MonoSpaced", Font.BOLD, 16));
 
-        //Grafico
-        XYChart chart = new XYChartBuilder()
-                .width(800)
-                .height(400)
-                .title("Line Chart")
-                .xAxisTitle("X")
-                .yAxisTitle("Y")
-                .build();
+        Button ok = new Button("OK");
+        ok.setBounds(150,150,150,150);
+        ok.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        ok.setFont(new Font("MonoSpaced", Font.BOLD, 16));
 
-        // Create the series and add data points
-        Series series = chart.addSeries("Data", new double[] { 1, 2, 3, 4, 5 }, new double[] { 1, 4, 9, 16, 25 });
+        //Lista
+        List list = new List(2);
+        list.add("Uniforme");
+        list.add("Normal");
+        //Choice
+        Choice choice = new Choice();
+        choice.add("Uniforme");
+        choice.add("Normal");
+        
+        //Evento botao 'OK'
+        ok.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+        //array recebe os inputs
+        values[0] = input01.getText();
+        values[1] = input02.getText();
+        values[2] = input03.getText();
+        values[3] = input04.getText();
+        values[4] = Integer.toString(choice.getSelectedIndex());
+        //values[4] = 1 normal; 0 uniforme
+        //DEBUG
+        System.out.println(values[0]); // inicio
+        System.out.println(values[1]); // fim
+        System.out.println(values[2]); // passo
+        System.out.println(values[3]); // pontos
+        System.out.println(values[4]); // Distribuição
+        
+        //todo Classe do Gabrigas
+        //Gabrigas.processarValores(values);
+        }});
 
-        // Customize the chart style
-        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNW);
-        JPanel chartPainel = new XChartPanel<>(chart);
-        /////////////////////////////////
 
+        //TALVEZ
+        //Canvas logo = new Canvas() {
+        //    public void paint(Graphics g) {
+        //        try {
+        //            Image image = ImageIO.read(new File("path/pra/foto")); // Coloque o caminho correto para a imagem
+        //            g.drawImage(image, 0, 1, null);
+        //       } catch (IOException e) {
+        //            e.printStackTrace();
+        //       }
+        //    }
+        //};
+        //pNorth.add(logo);
+        //f.add("Center",logo);
+        
+        
+        
+        
         //adicionando os componentes ao container Panel
+        pSouthW.add(input01Label);
+        pSouthW.add(input01);
+        pSouthW.add(input02Label);
+        pSouthW.add(input02);
+        pSouthW.add(input03Label);
+        pSouthW.add(input03);
+        pSouthW.add(input04Label);
+        pSouthW.add(input04);
+        pSouthE.add(none01);
+        pSouthE.add(choice);
+        pSouthE.add(ok);
+        pSouth.add("West",pSouthW);
+        pSouth.add("East",pSouthE);
 
-        pSouth.add(input01Label);
-        pSouth.add(input01);
-        pSouth.add(input02Label);
-        pSouth.add(input02);
-        pSouth.add(input03Label);
-        pSouth.add(input03);
-        pSouth.add(input04Label);
-        pSouth.add(input04);
-        pSouth.add(none0);
-        pSouth.add(none01);
-        pSouth.add(b1);
-        pSouth.add(b2);
+
         pNorth.add(algo);
-
-
-
+        
         f.add("South", pSouth);
         f.add("North", pNorth);
-//        f.add("West", pWest);
-//        f.add("East", pEast);
-        //f.add("North", l);
-        //f.add(l);
-        //f.add(b);
-        //f.pack();
-        f.add("Center",chartPainel);
+        f.add("West", pWest);
+        f.add("East", pEast);
         f.setVisible(true);
-
-    }
     
+    }
 }
-
 class Tratador implements WindowListener{
     public void windowClosing (WindowEvent we)
-    {
-        System.exit(0);
-    }
-    public void windowActivated (WindowEvent we){}
-    public void windowClosed (WindowEvent we){}
-    public void windowDeactivated (WindowEvent we){}
-    public void windowDeiconified(WindowEvent we){}
-    public void windowIconified (WindowEvent we){}
-    public void windowOpened (WindowEvent we){}
+   {
+      System.exit(0);
+   }
+   public void windowActivated (WindowEvent we){}
+   public void windowClosed (WindowEvent we){}
+   public void windowDeactivated (WindowEvent we){}
+   public void windowDeiconified(WindowEvent we){}
+   public void windowIconified (WindowEvent we){}
+   public void windowOpened (WindowEvent we){}
 }
+
+
